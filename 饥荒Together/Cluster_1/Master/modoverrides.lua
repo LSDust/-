ff8ec97da265c55a7cd41fb900e0085f42f54f3e
LSDust <2250919876@qq.com>
2018-12-03 22:48:52 +0800

    新建备份
    千珏中期秋

---
+++ 饥荒Together/Cluster_1/Master/modoverrides.lua
@@ -0,0 +1,77 @@
+return {
+  ["workshop-100010024"]={
+    configuration_options={
+      Craft="Normal",
+      Destroyable="yees",
+      FoodSpoilage=1,
+      Language="En",
+      Position="Center",
+      Slots=80 
+    },
+    enabled=true 
+  },
+  ["workshop-100010027"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-100010040"]={
+    configuration_options={
+      AUTOPAUSECONSOLE=false,
+      AUTOPAUSEMAP=false,
+      AUTOPAUSESINGLEPLAYERONLY=true,
+      ENABLECLIENTPAUSE=false,
+      ENABLEHOTKEY=true,
+      KEYBOARDTOGGLEKEY="P" 
+    },
+    enabled=true 
+  },
+  ["workshop-100010059"]={ configuration_options={  }, enabled=true },
+  ["workshop-100010062"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      send_unknwon_prefabs=true,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-2199027653598513601"]={ configuration_options={  }, enabled=true },
+  ["workshop-2199027653598513728"]={ configuration_options={  }, enabled=true },
+  ["workshop-2199027653598514255"]={ configuration_options={ penalty=0.2, uptime=3 }, enabled=true },
+  ["workshop-2199027653598516741"]={
+    configuration_options={
+      CHANGMAOKT_PF=false,
+      CHANGMAO_PF=false,
+      KING_DAMAGE=30,
+      KING_LV=40,
+      KING_PF=false,
+      REDSWORD_DAMAGE=10,
+      REDSWORD_LV=40,
+      REDSWORD_PF=false,
+      RUIWEN_DAMAGE=58,
+      RUIWEN_PF=false,
+      XIANGRIKUI_CHOP=false,
+      XIANGRIKUI_DAMAGE=55,
+      XIANGRIKUI_DIG=false,
+      XIANGRIKUI_FW=0,
+      XIANGRIKUI_HAMMER=false,
+      XIANGRIKUI_MINE=false,
+      XIANGRIKUI_NET=false,
+      XIANGRIKUI_PA=0,
+      XIANGRIKUI_PF=false,
+      XIANGRIKUI_QD=0,
+      XIANGRIKUI_RANG=1,
+      XIANGRIKUI_SPEED=1,
+      XIMICHANGMAOKT_DAMAGE=55,
+      XIMICHANGMAO_DAMAGE=30,
+      XIMICHANGMAO_LV=40,
+      XIMIGJ_DAMAGE=100,
+      XIMIGJ_PF=false,
+      XIMIJITA_DAMAGE=58,
+      XIMIJITA_PF=false,
+      XIMISZ_DAMAGE=58,
+      XIMISZ_PF=false 
+    },
+    enabled=true 
+  } 
+}
