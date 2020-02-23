27161c90ace41e2d53363d496d7f1dd70bb7c1d4
LSDust <2250919876@qq.com>
2020-02-23 18:35:26 +0800

    旧神归来
    人物沃利

--- 饥荒Together/Cluster_1/Master/modoverrides.lua
+++ 饥荒Together/Cluster_1/Master/modoverrides.lua
@@ -1,4 +1,8 @@
 return {
+  ["workshop-100010021"]={
+    configuration_options={ food_estimation=-1, food_order=0, food_style=0, show_food_units=-1 },
+    enabled=true 
+  },
   ["workshop-100010024"]={
     configuration_options={
       Craft="Normal",
@@ -10,7 +14,6 @@ return {
     },
     enabled=true 
   },
-  ["workshop-100010027"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
   ["workshop-100010040"]={
     configuration_options={
       AUTOPAUSECONSOLE=false,
@@ -22,21 +25,5 @@ return {
     },
     enabled=true 
   },
-  ["workshop-100010059"]={ configuration_options={  }, enabled=true },
-  ["workshop-100010062"]={
-    configuration_options={
-      divider=5,
-      random_health_value=0,
-      random_range=0,
-      send_unknwon_prefabs=true,
-      show_type=0,
-      unknwon_prefabs=1,
-      use_blacklist=true 
-    },
-    enabled=true 
-  },
-  ["workshop-2199027653598513601"]={ configuration_options={  }, enabled=true },
-  ["workshop-2199027653598514255"]={ configuration_options={ penalty=0.2, uptime=3 }, enabled=true },
-  ["workshop-2199027653598514364"]={ configuration_options={  }, enabled=true },
-  ["workshop-2199027653598518049"]={ configuration_options={ language=2 }, enabled=true } 
+  ["workshop-2199027653598513601"]={ configuration_options={  }, enabled=true } 
 }
