e298f67aeafd8c00acb3d63811600cd8251518a9
LSDust <2250919876@qq.com>
2020-03-08 14:27:51 +0800

    神话书说
    入夏

---
+++ 饥荒Together/Cluster_2/Caves/modoverrides.lua
@@ -0,0 +1,15 @@
+return {
+  ["workshop-100010040"]={
+    configuration_options={
+      AUTOPAUSECONSOLE=false,
+      AUTOPAUSEMAP=false,
+      AUTOPAUSESINGLEPLAYERONLY=true,
+      ENABLECLIENTPAUSE=false,
+      ENABLEHOTKEY=true,
+      KEYBOARDTOGGLEKEY="P" 
+    },
+    enabled=true 
+  },
+  ["workshop-2199027653598520427"]={ configuration_options={ Skillbadge=true }, enabled=true },
+  ["workshop-2199027653598524334"]={ configuration_options={ Language=false }, enabled=true } 
+}
